f73b8d82c6a1c96b08cf44588263b28b774a14ad
stanleyndaba <your-email@example.com>
2026-02-15 05:19:46 +0200

    Fix Products mega menu clipping and enforce solid background

--- src/components/landing/ProductsMegaMenu.tsx
+++ src/components/landing/ProductsMegaMenu.tsx
@@ -38,7 +38,7 @@ export function ProductsMegaMenu() {
                         Products
                     </NavigationMenuTrigger>
                     <NavigationMenuContent>
-                        <div className="flex flex-col lg:flex-row w-[calc(100vw-2rem)] md:w-[850px] lg:w-[1100px] bg-[#020202] [backdrop-filter:blur(24px)_saturate(150%)] rounded-xl overflow-hidden shadow-[0_30px_100px_rgba(0,0,0,0.9)] border border-white/10 lg:-translate-x-[250px]">
+                        <div className="flex flex-col lg:flex-row w-[calc(100vw-2rem)] md:w-[850px] lg:w-[1100px] bg-[#050505] rounded-xl overflow-hidden shadow-[0_30px_100px_rgba(0,0,0,1)] border border-white/10 lg:-translate-x-[120px] relative z-[100]">
 
                             {/* Column 1: Audit Vectors */}
                             <div className="flex-[1.2] p-8 lg:p-10 border-r border-white/5">
